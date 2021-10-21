30a97847cb78afb1b82984f9190dcac05ac0ce21
Douglas Quintiliano dos Santos <douglas.q.santos@gmail.com>
2021-10-21 18:50:45 -0300

    :white_check_mark: Adding integration Test

---
+++ data/integration_test.go
@@ -0,0 +1,390 @@
+//go:build integration
+
+// run test with this command: go test . --tags integration --count=1
+// run test with this command: go test -v . --tags integration --count=1
+// run test with this command to send output to web: go test -coverprofile=coverage.out . --tags integration
+// go tool cover -html=coverage.out
+
+package data
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"testing"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
+)
+
+var (
+	host     = "localhost"
+	user     = "postgres"
+	password = "secret"
+	dbName   = "celeritas_test"
+	port     = "5435"
+	dsn      = "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable timezone=UTC connect_timeout=5"
+)
+
+var dummyUser = User{
+	FirstName: "Some",
+	LastName:  "Guy",
+	Email:     "me@here.com",
+	Active:    1,
+	Password:  "password",
+}
+
+var models Models
+var testDB *sql.DB
+var resource *dockertest.Resource
+var pool *dockertest.Pool
+
+func TestMain(m *testing.M) {
+	os.Setenv("DATABASE_TYPE", "postgres")
+
+	p, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("could not connect to docker: %s", err)
+	}
+
+	pool = p
+
+	opts := dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "13.4",
+		Env: []string{
+			"POSTGRES_USER=" + user,
+			"POSTGRES_PASSWORD=" + password,
+			"POSTGRES_DB=" + dbName,
+		},
+		ExposedPorts: []string{"5432"},
+		PortBindings: map[docker.Port][]docker.PortBinding{
+			"5432": {
+				{
+					HostIP:   "0.0.0.0",
+					HostPort: port,
+				},
+			},
+		},
+	}
+
+	resource, err = pool.RunWithOptions(&opts)
+	if err != nil {
+		_ = pool.Purge(resource)
+		log.Fatalf("could not start resource: %s", err)
+	}
+
+	if err := pool.Retry(func() error {
+		var err error
+		testDB, err = sql.Open("pgx", fmt.Sprintf(dsn, host, port, user, password, dbName))
+		if err != nil {
+			log.Println("Error:", err)
+			return err
+		}
+		return testDB.Ping()
+	}); err != nil {
+		_ = pool.Purge(resource)
+		log.Fatalf("could not connect to docker : %s", err)
+	}
+
+	err = createTables(testDB)
+	if err != nil {
+		log.Fatalf("error creating tables: %s", err)
+	}
+
+	models = New(testDB)
+	code := m.Run()
+
+	if err = pool.Purge(resource); err != nil {
+		log.Fatalf("could not purge resource: %s", err)
+	}
+
+	os.Exit(code)
+
+}
+
+func createTables(db *sql.DB) error {
+	stmt :=
+		`CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+	RETURNS TRIGGER AS $$
+	BEGIN
+		NEW.updated_at = NOW();
+	RETURN NEW;
+	END;
+	$$ LANGUAGE plpgsql;
+	
+	drop table if exists users cascade;
+	
+	CREATE TABLE users (
+			id SERIAL PRIMARY KEY,
+			first_name character varying(255) NOT NULL,
+			last_name character varying(255) NOT NULL,
+			user_active integer NOT NULL DEFAULT 0,
+			email character varying(255) NOT NULL UNIQUE,
+			password character varying(60) NOT NULL,
+			created_at timestamp without time zone NOT NULL DEFAULT now(),
+			updated_at timestamp without time zone NOT NULL DEFAULT now()
+	);
+	
+	CREATE TRIGGER set_timestamp
+			BEFORE UPDATE ON users
+			FOR EACH ROW
+			EXECUTE PROCEDURE trigger_set_timestamp();
+	
+	drop table if exists remember_tokens;
+	
+	CREATE TABLE remember_tokens (
+			id SERIAL PRIMARY KEY,
+			user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+			remember_token character varying(100) NOT NULL,
+			created_at timestamp without time zone NOT NULL DEFAULT now(),
+			updated_at timestamp without time zone NOT NULL DEFAULT now()
+	);
+	
+	CREATE TRIGGER set_timestamp
+			BEFORE UPDATE ON remember_tokens
+			FOR EACH ROW
+			EXECUTE PROCEDURE trigger_set_timestamp();
+	
+	drop table if exists tokens;
+	
+	CREATE TABLE tokens (
+			id SERIAL PRIMARY KEY,
+			user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+			first_name character varying(255) NOT NULL,
+			email character varying(255) NOT NULL,
+			token character varying(255) NOT NULL,
+			token_hash bytea NOT NULL,
+			created_at timestamp without time zone NOT NULL DEFAULT now(),
+			updated_at timestamp without time zone NOT NULL DEFAULT now(),
+			expiry timestamp without time zone NOT NULL
+	);
+	
+	CREATE TRIGGER set_timestamp
+			BEFORE UPDATE ON tokens
+			FOR EACH ROW
+			EXECUTE PROCEDURE trigger_set_timestamp();
+	`
+
+	_, err := db.Exec(stmt)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func TestUser_Table(t *testing.T) {
+	s := models.Users.Table()
+	if s != "users" {
+		t.Error("wrong table name returned: ", s)
+	}
+}
+
+func TestUser_Insert(t *testing.T) {
+	id, err := models.Users.Insert(dummyUser)
+	if err != nil {
+		t.Error("failed to insert user: ", err)
+	}
+	if id == 0 {
+		t.Error("0 returned as id after insert")
+	}
+}
+
+func TestUser_Get(t *testing.T) {
+	u, err := models.Users.Get(1)
+	if err != nil {
+		t.Error("failed to get user: ", err)
+	}
+
+	if u.ID == 0 {
+		t.Error("id of returned user is 0: ", err)
+	}
+}
+
+func TestUser_GetAll(t *testing.T) {
+	_, err := models.Users.GetAll()
+	if err != nil {
+		t.Error("failed to get users: ", err)
+	}
+}
+
+func TestUser_GetByEmail(t *testing.T) {
+	u, err := models.Users.GetByEmail("me@here.com")
+	if err != nil {
+		t.Error("failed to get user: ", err)
+	}
+
+	if u.ID == 0 {
+		t.Error("id of returned user is 0: ", err)
+	}
+}
+
+func TestUser_Update(t *testing.T) {
+	u, err := models.Users.Get(1)
+	if err != nil {
+		t.Error("failed to get user: ", err)
+	}
+
+	u.LastName = "Smith"
+	err = u.Update(*u)
+	if err != nil {
+		t.Error("failed to update user: ", err)
+	}
+
+	u, err = models.Users.Get(1)
+	if err != nil {
+		t.Error("failed to get user: ", err)
+	}
+
+	if u.LastName != "Smith" {
+		t.Error("last name not updated in database")
+	}
+}
+
+func TestUser_PasswordMatches(t *testing.T) {
+	u, err := models.Users.Get(1)
+	if err != nil {
+		t.Error("failed to get user: ", err)
+	}
+
+	matches, err := u.PasswordMatches("password")
+	if err != nil {
+		t.Error("error checking match: ", err)
+	}
+
+	if !matches {
+		t.Error("password does not match when it should")
+	}
+
+	matches, err = u.PasswordMatches("123")
+	if err != nil {
+		t.Error("error checking match: ", err)
+	}
+
+	if matches {
+		t.Error("password matches when it should not")
+	}
+}
+
+func TestUser_ResetPassword(t *testing.T) {
+	err := models.Users.ResetPassword(1, "new_password")
+	if err != nil {
+		t.Error("error resetting password: ", err)
+	}
+
+	err = models.Users.ResetPassword(2, "new_password")
+	if err == nil {
+		t.Error("did not get an error when trying to reset password for non-existing user.")
+	}
+
+}
+
+func TestUser_Delete(t *testing.T) {
+	err := models.Users.Delete(1)
+	if err != nil {
+		t.Error("failed to delete user: ", err)
+	}
+
+	_, err = models.Users.Get(1)
+	if err == nil {
+		t.Error("retrieved user who was supposed to be deleted")
+	}
+}
+
+func TestToken_Table(t *testing.T) {
+	s := models.Tokens.Table()
+	if s != "tokens" {
+		t.Error("wrong table name returned for tokens")
+	}
+}
+
+func TestToken_GenerateToken(t *testing.T) {
+	id, err := models.Users.Insert(dummyUser)
+	if err != nil {
+		t.Error("error inserting user: ", err)
+	}
+
+	_, err = models.Tokens.GenerateToken(id, time.Hour*24*365)
+	if err != nil {
+		t.Error("error generating token: ", err)
+	}
+}
+
+func TestToken_Insert(t *testing.T) {
+	u, err := models.Users.GetByEmail(dummyUser.Email)
+	if err != nil {
+		t.Error("failed to get user")
+	}
+	token, err := models.Tokens.GenerateToken(u.ID, time.Hour*24*365)
+	if err != nil {
+		t.Error("error generating token: ", err)
+	}
+
+	err = models.Tokens.Insert(*token, *u)
+	if err != nil {
+		t.Error("error inserting token: ", err)
+	}
+}
+
+func TestToken_GetUserForToken(t *testing.T) {
+	token := "abc"
+	_, err := models.Tokens.GetUserForToken(token)
+	if err == nil {
+		t.Error("error expected but not received when getting user with a bad token")
+	}
+
+	u, err := models.Users.GetByEmail(dummyUser.Email)
+	if err != nil {
+		t.Error("failed to get user")
+	}
+
+	_, err = models.Tokens.GetUserForToken(u.Token.PlainText)
+	if err != nil {
+		t.Error("failed to get user with valid token: ", err)
+	}
+}
+
+func TestToken_GetTokensForUser(t *testing.T) {
+	tokens, err := models.Tokens.GetTokensForUser(1)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(tokens) > 0 {
+		t.Error("tokens returned for non-existent user")
+	}
+}
+
+func TestToken_Get(t *testing.T) {
+	u, err := models.Users.GetByEmail(dummyUser.Email)
+	if err != nil {
+		t.Error("failed to get user")
+	}
+
+	_, err = models.Tokens.Get(u.Token.ID)
+	if err != nil {
+		t.Error("error getting token by id: ", err)
+	}
+}
+
+func TestToken_GetByToken(t *testing.T) {
+	u, err := models.Users.GetByEmail(dummyUser.Email)
+	if err != nil {
+		t.Error("failed to get user")
+	}
+
+	_, err = models.Tokens.GetByToken(u.Token.PlainText)
+	if err != nil {
+		t.Error("error getting token by token: ", err)
+	}
+
+	_, err = models.Tokens.GetByToken("123")
+	if err == nil {
+		t.Error("error getting non-existing token by token: ", err)
+	}
+}
---
+++ data/models_test.go
@@ -0,0 +1,48 @@
+package data
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	db2 "github.com/upper/db/v4"
+
+	"github.com/DATA-DOG/go-sqlmock"
+)
+
+func TestNew(t *testing.T) {
+	fakeDB, _, _ := sqlmock.New()
+	defer fakeDB.Close()
+
+	_ = os.Setenv("DATABASE_TYPE", "postgres")
+	m := New(fakeDB)
+	if fmt.Sprintf("%T", m) != "data.Models" {
+		t.Error("Wrong type", fmt.Sprintf("%T", m))
+	}
+
+	_ = os.Setenv("DATABASE_TYPE", "mysql")
+	m = New(fakeDB)
+	if fmt.Sprintf("%T", m) != "data.Models" {
+		t.Error("Wrong type", fmt.Sprintf("%T", m))
+	}
+
+}
+
+func TestGetInsertID(t *testing.T) {
+	var id db2.ID
+	// PostgreSQL id type
+	id = int64(1)
+
+	returnedID := getInsertID(id)
+	if fmt.Sprintf("%T", returnedID) != "int" {
+		t.Error("wrong type returned")
+	}
+
+	// MySQL/MariaDB id type
+	id = 1
+	returnedID = getInsertID(id)
+	if fmt.Sprintf("%T", returnedID) != "int" {
+		t.Error("wrong type returned")
+	}
+
+}
